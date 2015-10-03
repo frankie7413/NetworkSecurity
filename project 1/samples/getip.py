e63d083f08b8d8628e8027ad5ae3cb3e1404ca16
sokk19xx <kourun@csu.fullerton.edu>
2015-10-02 23:22:26 -0700

    Update getip.py

--- project 1/samples/getip.py
+++ project 1/samples/getip.py
@@ -1,33 +1,48 @@
+
 ############################################
 # This file gives function getifip() which
 # you can use to learn the IP address of the
-# local system.
-# Some code borrowed from:
-# http://coderstalk.blogspot.com/2011/04/python-code-to-get-ip-address-from.html
+# first interface whose IP is not 127.0.0.1
+# IMPORTANT: you need to run pip install 
+# netifaces for this to work.
 ############################################
 
 # The necessary files
 import socket, fcntl, struct
+import netifaces
 
 #############################################
-# Retrieves the ip of the given network card.
-# In this assignment, you may assume that 
-# network adapter "eth0" is the one that is
-# always being used.
-# @param ifn - the name of the network card
-# In this assignment, you can always set it
-# to "eth0"
+# Retrieves the ip of the network card with 
+# an IPv4 address that is not 127.0.0.1.
 # @return - the string containing the IP 
-# address of the current system.
+# address of the network adapter that is not
+# if the IP is not 127.0.0.1; returns None
+# if no such interface is detected
 ##############################################
-def getifip(ifn):
-    	# Open the socket 
-	sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def getifip():
+
+	# Get all the network interfaces on the system
+	networkInterfaces = netifaces.interfaces()
 	
-	# Grab the IP address of the current 
-	# interface from the kernel. Convert
-	# the original format to string format.
-	return socket.inet_ntoa(fcntl.ioctl(sck.fileno(),0x8915,struct.pack(b'256s', ifn[:15]))[20:24])
+	# The IP address
+	ipAddr = None
+	
+	# Go through all the interfaces
+	for netFace in networkInterfaces:
+		
+		# The IP address of the interface
+		addr = netifaces.ifaddresses(netFace)[2][0]['addr'] 
+		
+		# Get the IP address
+		if not addr == "127.0.0.1":
+			
+			# Save the IP addrss and break
+			ipAddr = addr
+			break	 
+			
+	return ipAddr
 
 
-print("The ip of the current system is: " + getifip(b"eth0"))
+print getifip()	
+
+#print("The ip of the current system is: " + getifip(b"eth0"))
